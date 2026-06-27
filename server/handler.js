c3318a5f15928f6dfab2714014a6c1c15ff06860
omshaurya <omshaurya1@gmail.com>
2026-06-27 15:10:22 +0530

    fix vercel routing: move handler out of api/ folder

--- server/handler.js
+++ server/handler.js
@@ -1,8 +1,8 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import connectDB from "../src/config/db.js";
-import app from "../src/app.js";
+import connectDB from "./src/config/db.js";
+import app from "./src/app.js";
 
 // Connect to MongoDB on cold start (cached across warm invocations)
 connectDB();
