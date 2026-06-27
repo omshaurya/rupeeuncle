15122b03c99c902cdccf5ff22d016002121bf4db
omshaurya <omshaurya1@gmail.com>
2026-06-27 13:02:03 +0530

    upadting

---
+++ server/api/index.js
@@ -0,0 +1,10 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import connectDB from "../src/config/db.js";
+import app from "../src/app.js";
+
+// Connect to MongoDB on cold start (cached across warm invocations)
+connectDB();
+
+export default app;
